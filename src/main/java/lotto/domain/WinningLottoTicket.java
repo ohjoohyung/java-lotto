04764b795c1f606514f7051589715c473e735765
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-02-28 16:17:28 +0900

    refactor: 필요없는 메서드 삭제

--- src/main/java/lotto/domain/WinningLottoTicket.java
+++ src/main/java/lotto/domain/WinningLottoTicket.java
@@ -32,7 +32,6 @@ public class WinningLottoTicket {
     public LottoRank compareNumbers(LottoTicket lottoTicket) {
         int matchCounts = lottoTicket.getMatchCounts(this.lottoTicket);
         boolean isBonusBall = lottoTicket.contains(bonusBallNumber);
-        System.out.println(matchCounts + "/" + isBonusBall);
         return LottoRank.of(matchCounts, isBonusBall);
     }
 }
